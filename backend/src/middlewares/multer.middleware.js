ff7434779a8e3bb99b455f250d2b48e7f5fdd72f
raaaghavv <raghavgoel454@gmail.com>
2026-01-31 16:23:02 +0530

    add multer for file uploads

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,24 @@
+import multer from "multer";
+import fs from "fs";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let uploadPath = "./uploads";
+
+    if (file.fieldname === "audioFile") {
+      uploadPath = "./uploads/audio";
+    } else if (file.fieldname === "coverImage") {
+      uploadPath = "./uploads/images";
+    }
+
+    // Create directory if it doesn't exist
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { uploadSong } from "../controllers/song.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
@@ -7,6 +8,12 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "welcome to synth!" });
 });
 
-router.route("/upload").post(uploadSong);
+router.route("/upload").post(
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "audioFile", maxCount: 1 },
+  ]),
+  uploadSong
+);
 
 export default router;
