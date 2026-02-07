dee5a7a6fdd7264233a2470656e222f2641a1992
raaaghavv <raghavgoel454@gmail.com>
2026-02-07 15:24:22 +0530

    create a global provider for the app

---
+++ frontend/src/components/Providers.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { PlayerProvider } from "@/context/PlayerContext";
+
+export default function Providers({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PlayerProvider>{children}</PlayerProvider>
+    </QueryClientProvider>
+  );
+}
