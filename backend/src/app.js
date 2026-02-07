a6cda74a25fd2ce1fe028213af4fec303733ce15
raaaghavv <raghavgoel454@gmail.com>
2026-02-07 15:21:28 +0530

    allow media to be accessed by browser

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  })
+);
 app.use("/uploads/images", express.static("uploads/images"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
